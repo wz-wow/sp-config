bf4a23dd198213c8a20d686fe95f45c676b14b1b
wz-wow <60765925+wz-wow@users.noreply.github.com>
2020-02-07 14:59:29 +0800

    Update Test01.java

--- test/src/test/Test01.java
+++ test/src/test/Test01.java
@@ -3,6 +3,7 @@ package test;
 public class Test01 {
 	
 	public static void main(String[] args) {
-		System.out.println("��ã�");
+		System.out.println("ÄãºÃ£¡");
+		//111111111111111111111111111
 	}
 }
